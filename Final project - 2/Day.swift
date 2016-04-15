f5c306909589293d841e38477684de7ab206c6ea
Student <student@bluc02mg0l9fh51.local>
2016-04-15 12:09:04 -0500

    add custom class

---
+++ Final project - 2/Day.swift
@@ -0,0 +1,14 @@
+//
+//  Day.swift
+//  Final project - 2
+//
+//  Created by Student on 4/15/16.
+//  Copyright © 2016 Jason Lucas. All rights reserved.
+//
+
+import UIKit
+
+class Day: NSObject {
+    
+
+}
--- Final project - 2/DetailedViewController.swift
+++ Final project - 2/DetailedViewController.swift
@@ -10,26 +10,13 @@ import UIKit
 
 class DetailedViewController: UIViewController {
 
+    @IBOutlet weak var foodTextField: UITextField!
+    @IBOutlet weak var caloriesTextField: UITextField!
+    
+    var days : Days!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
