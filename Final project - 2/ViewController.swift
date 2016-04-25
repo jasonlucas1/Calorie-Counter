1124f5c86ff1ba63d6fb811ef1e0bd01594a7830
Jason Lucas <jasonlucas@jasons-macbook-air.local>
2016-04-25 12:12:47 -0500

    Removed edit button

--- Final project - 2/ViewController.swift
+++ Final project - 2/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
     @IBOutlet weak var tableView: UITableView!
-    var days = ["Today"]
+    var days = ["Today", "Tommorow"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
